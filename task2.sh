321e99e2ae5cbfe3c361fa550501c122e2ddbe2a
Faraifarrell <faraifarouk@gmail.com>
2024-05-17 20:21:36 +0200

    task2.sh

---
+++ task2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Print "hello World"
+echo "Hello, World" 
+
+#Print current date
+echo "Today is: $(date)"
+
+#Print Username
+echo "Yor are logged in as: $(whoami)"
+
+#Print current working directory
+echo "Your current working directory is: $(pwd)"
+
+
